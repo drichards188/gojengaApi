8477e9781e6b6952c3659df1d6fc46bc58081f1c
drichards188 <github@cxoprotect.com>
2023-01-19 12:08:43 -0700

    all account tests working

--- src/gojenga/test_main.py
+++ src/gojenga/test_main.py
@@ -1,29 +1,54 @@
 from fastapi.testclient import TestClient
 
+from decimal import *
 from main import app
 from handlers.user_handler import UserHandler
+from handlers.account_handler import AccountHandler
 
 client = TestClient(app)
 
 
 def test_get_user():
-    response = client.get("/handlers/kovax", headers={'Is-Test': 'True'})
+    response = client.get("/user/kovax", headers={'Is-Test': 'True'})
     assert response.status_code == 200, f'wanted 200 got {response.status_code}'
     assert response.json() == {'response': {'name': 'kovax', 'password': '5182'}}
 
 
 def test_create_user():
-    response = client.post("/handlers", json={"name": "kovax", "password": "5182"}, headers={'Is-Test': 'True'})
-    assert response.json() == {"response": "insert item success"}
+    response = client.post("/user", json={"name": "kovax", "password": "5182"}, headers={'Is-Test': 'True'})
+    assert response.json() == {'response': 'insert item succeeded'}
 
 
 def test_update_user():
-    response = client.put("/handlers/david", json={"name": "david", "password": "0000"}, headers={'Is-Test': 'True'})
+    response = client.put("/user/david", json={"name": "david", "password": "0000"}, headers={'Is-Test': 'True'})
     assert response.json() == {"response": "update item success"}
 
 
 def test_delete_user():
     resp = UserHandler.handle_create_user('zala', '5821', True)
-    assert resp == "insert item success", 'failed to insert item before testing delete'
-    response = client.delete("/handlers/zala", headers={'Is-Test': 'True'})
+    assert resp == "insert item succeeded", 'failed to insert item before testing delete'
+    response = client.delete("/user/zala", headers={'Is-Test': 'True'})
+    assert response.json() == {"response": "delete item success"}
+
+
+def test_get_account():
+    response = client.get("/account/kovax", headers={'Is-Test': 'True'})
+    assert response.status_code == 200, f'wanted 200 got {response.status_code}'
+    assert response.json() == {'response': {'name': 'kovax', 'balance': 235.99}}
+
+
+def test_create_account():
+    response = client.post("/account", json={"name": "kovax", 'balance': 235.99}, headers={'Is-Test': 'True'})
+    assert response.json() == {'response': 'insert item succeeded'}
+
+
+def test_update_account():
+    response = client.put("/account/david", json={"name": "david", "balance": 235.99}, headers={'Is-Test': 'True'})
+    assert response.json() == {"response": "update item success"}
+
+
+def test_delete_account():
+    resp = AccountHandler.handle_create_account('zala', Decimal('212.38'), True)
+    assert resp == "insert item succeeded", 'failed to insert item before testing delete'
+    response = client.delete("/account/zala", headers={'Is-Test': 'True'})
     assert response.json() == {"response": "delete item success"}
